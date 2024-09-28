11dfad4808534de66d6ce92e128b1969e255719a
Fan Pu <772700563@qq.com>
2024-09-27 20:27:32 -0500

    need to alter objective function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from toy_test import *
 
 if __name__ == '__main__':
-    test_case = ToyTest(5, 2, 1, 1)
-    test_case.solve()
+    test_case = ToyTest(3, 1, 0, 1)
     test_case.visualize()
+    # plt.show()
+    test_case.solve()
     test_case.visualize_routes()
     plt.show()
     ss = 0
--- toy_test.py
+++ toy_test.py
@@ -7,22 +7,29 @@ import networkx as nx
 from gurobipy import GRB
 
 drone_min_t = 5
-drone_max_t = 20
+drone_max_t = 10
 truck_min_t = 10
 truck_max_t = 50
-M = 9999999
-drone_endurance = 30
-drone_max_weight = 3
+M = 99999
+epsilon = 0.01
+drone_endurance = 15
+drone_max_weight = 50
 drone_max_volume = 10
 truck_max_weight = 500
 truck_max_drone_dock_num = 10
 
-hub_cost_min = 500
-hub_cost_max = 1000
+hub_cost_min = 1
+hub_cost_max = 2
+
+demand_weight_max = 5
+demand_weight_min = 1
+demand_volume_max = 3
+demand_volume_min = 1
 
 
 class ToyTest:
     def __init__(self, num_customers, num_hubs, num_trucks, num_drones):
+        self.constraints = []
         self.num_customers = num_customers
         self.num_hubs = num_hubs
         self.num_trucks = num_trucks
@@ -91,7 +98,16 @@ class ToyTest:
         # Randomly generate additional arcs for truck routes (between depot_source, hubs, and customers)
         all_locations = customers + hubs
         for i in range(len(all_locations)):
-            for j in range(i + 1, len(all_locations)):
+            for j in range(len(all_locations)):
+                if j == i:
+                    continue
+                if (i == 2 and j == 4) or (i == 2 and j == 0):
+                    truck_travel_time = random.randint(truck_min_t, truck_max_t)
+                    drone_travel_time = random.randint(drone_min_t, drone_max_t)
+                    G.add_edge(all_locations[i], all_locations[j],
+                               travel_time={'truck': truck_travel_time, 'drone': drone_travel_time})
+                    G.add_edge(all_locations[j], all_locations[i],
+                               travel_time={'truck': truck_travel_time, 'drone': drone_travel_time})
                 if random.random() > 0.5:  # Randomly decide if a arc exists
                     truck_travel_time = random.randint(truck_min_t, truck_max_t)
                     drone_travel_time = random.randint(drone_min_t, drone_max_t)
@@ -104,14 +120,21 @@ class ToyTest:
         for hub in hubs:
             for customer in customers:
                 if random.random() > 0.5:  # Randomly decide if a drone arc exists
-                    truck_travel_time = random.randint(truck_min_t, truck_max_t)
-                    drone_travel_time = random.randint(drone_min_t, drone_max_t)
+                    truck_travel_time = random.randint(truck_min_t, truck_max_t) * 0.1
+                    drone_travel_time = random.randint(drone_min_t, drone_max_t) * 0.1
                     G.add_edge(hub, customer, travel_time={'truck': truck_travel_time, 'drone': drone_travel_time})
                     G.add_edge(customer, hub, travel_time={'truck': truck_travel_time, 'drone': drone_travel_time})
 
         self.G = G
-        self.demand_weights = {self.all_nodes_indices[n_name]: random.uniform(0, 5) for n_name in customers}
-        self.demand_volume = {self.all_nodes_indices[n_name]: random.uniform(0, 3) for n_name in customers}
+        self.demand_weights = {}
+        self.demand_volume = {}
+        for n_name in customers:
+            n = self.all_nodes_indices[n_name]
+            self.demand_weights[n] = random.uniform(demand_weight_min, demand_weight_max)
+            self.demand_volume[n] = random.uniform(demand_volume_min, demand_volume_max)
+        for s_name in hubs:
+            s = self.all_nodes_indices[s_name]
+            self.demand_weights[s] = self.demand_volume[s] = epsilon
         print(self.demand_weights)
         print(self.demand_volume)
 
@@ -132,22 +155,32 @@ class ToyTest:
                 node_colors.append('gray')  # Default color for others (if any)
 
         # Draw the nodes and edges
-        nx.draw(self.G, pos, with_labels=True, node_color=node_colors, node_size=1000, font_size=10, font_weight='bold',
+        nx.draw(self.G, pos, with_labels=True, node_color=node_colors, node_size=500, font_size=10, font_weight='bold',
                 edge_color='gray')
+
+        # Create custom labels for nodes (you can adjust this to display specific attributes)
+        node_labels = {}
+        for node in self.G.nodes():
+            if node in self.customers:
+                n = self.all_nodes_indices[node]
+                node_labels[node] = f"w:{self.demand_weights[n]:.2f}|v:{self.demand_volume[n]:.2f}"
+
+        label_pos = {node: (x, y + 0.05) for node, (x, y) in pos.items()}  # Adjust 0.05 to control the offset
+        # Draw the node labels
+        nx.draw_networkx_labels(self.G, label_pos, labels=node_labels, font_size=10)
 
         # Create labels for travel times on edges
         edge_labels = {}
         for u, v, data in self.G.edges(data=True):
             travel_times = data['travel_time']
             # Format the travel times for display
-            edge_labels[(u, v)] = f"T: {travel_times['truck']} | D: {travel_times['drone']}"
+            edge_labels[(u, v)] = f"T: {travel_times['truck']:.2f} | D: {travel_times['drone']:.2f}"
 
         # Draw edge labels (travel times for both truck and drone)
         nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels, font_size=8)
 
     def solve(self):
         model = gp.Model("model")
-        self.constraints = []
         # add decision variables
         x_list = {}
         y_list = {}
@@ -177,13 +210,17 @@ class ToyTest:
         w_list = {}
         v_list = {}
         c_list = {}
+        ad_list = {}
+        ak_list = {}
         for n in range(len(self.all_nodes_indices)):
             for d in range(self.num_drones):
                 t_list[(n, d)] = model.addVar(name=f"t_{(n, d)}", vtype=GRB.CONTINUOUS, lb=0)
                 w_list[(n, d)] = model.addVar(name=f"w_{(n, d)}", vtype=GRB.CONTINUOUS, lb=0)
                 v_list[(n, d)] = model.addVar(name=f"v_{(n, d)}", vtype=GRB.CONTINUOUS, lb=0)
+                ad_list[(n, d)] = model.addVar(name=f"ad_{(n, d)}", vtype=GRB.CONTINUOUS, lb=0)
             for k in range(self.num_trucks):
                 c_list[(n, k)] = model.addVar(name=f"c_{(n, k)}", vtype=GRB.CONTINUOUS, lb=0)
+                ak_list[(n, k)] = model.addVar(name=f"ak_{(n, k)}", vtype=GRB.CONTINUOUS, lb=0)
 
         # add objective function
         obj_expr = 0
@@ -426,7 +463,7 @@ class ToyTest:
                 self.constraints.append(model.addConstr(w_list[(n, d)] <= drone_max_weight, f"drone_load1_{id}"))
                 id += 1
         # cons 2
-        for n_name in self.customers:
+        for n_name in self.customers + self.hubs:
             n = self.all_nodes_indices[n_name]
             in_arcs = self.G.in_edges(n_name)
             for i_name, _ in in_arcs:
@@ -442,7 +479,7 @@ class ToyTest:
                 self.constraints.append(model.addConstr(v_list[(n, d)] <= drone_max_volume, f"drone_load3_{id}"))
                 id += 1
         # cons 4
-        for n_name in self.customers:
+        for n_name in self.customers + self.hubs:
             n = self.all_nodes_indices[n_name]
             in_arcs = self.G.in_edges(n_name)
             for i_name, _ in in_arcs:
@@ -461,7 +498,7 @@ class ToyTest:
                 self.constraints.append(model.addConstr(c_list[(n, k)] <= truck_max_weight, f"truck_load1_{id}"))
                 id += 1
         # cons 2
-        for n_name in self.customers:
+        for n_name in self.customers + self.hubs:
             n = self.all_nodes_indices[n_name]
             in_arcs = self.G.in_edges(n_name)
             for i_name, _ in in_arcs:
@@ -471,16 +508,6 @@ class ToyTest:
                     self.constraints.append(model.addConstr(c_list[(n, k)] >= rhs, f"truck_load2_{id}"))
                     id += 1
         # cons 3
-        for s_name in self.hubs:
-            s = self.all_nodes_indices[s_name]
-            in_arcs = self.G.in_edges(s_name)
-            for i_name, _ in in_arcs:
-                i = self.all_nodes_indices[i_name]
-                for k in range(self.num_trucks):
-                    rhs = c_list[(i, k)] + M * (x_list[(i, s, k)] + f_list[(i, s, k)] - 1)
-                    self.constraints.append(model.addConstr(c_list[(s, k)] >= rhs, f"truck_load3_{id}"))
-                    id += 1
-        # cons 4
         for i_name, j_name, _ in edges:
             i = self.all_nodes_indices[i_name]
             j = self.all_nodes_indices[j_name]
@@ -488,34 +515,86 @@ class ToyTest:
                 lhs = 0
                 for d in range(self.num_drones):
                     lhs += z_list[(i, j, k, d)]
-                self.constraints.append(model.addConstr(lhs <= truck_max_drone_dock_num, f"truck_load4_{id}"))
+                self.constraints.append(model.addConstr(lhs <= truck_max_drone_dock_num, f"truck_load3_{id}"))
                 id += 1
 
+        # drone truck synchronization ************************************************************
+        id = 0
+        # cons 1 and 2
+        for n_name in self.customers + self.hubs + [self.depot_source]:
+            if n_name == self.depot_sink:
+                continue
+            n = self.all_nodes_indices[n_name]
+            out_arcs = self.G.out_edges(n_name, data=True)
+            for _, j_name, data in out_arcs:
+                j = self.all_nodes_indices[j_name]
+                for d in range(self.num_drones):
+                    # cons 1
+                    travel_time = data["travel_time"]["drone"]
+                    rhs = ad_list[(n, d)] + travel_time + M * (y_list[(n, j, d)] - 1)
+                    self.constraints.append(model.addConstr(ad_list[(j, d)] >= rhs, f"sync1_{id}"))
+                    id += 1
+                for k in range(self.num_trucks):
+                    # cons 2
+                    travel_time = data["travel_time"]["truck"]
+                    rhs = ak_list[(n, k)] + travel_time + M * (x_list[(n, j, k)] + f_list[(n, j, k)] - 1)
+                    self.constraints.append(model.addConstr(ak_list[(j, k)] >= rhs, f"sync2_{id}"))
+                    id += 1
+        # cons 3 and 4
+        for s_name in self.hubs:
+            s = self.all_nodes_indices[s_name]
+            out_arcs = self.G.out_edges(s_name, data=True)
+            for _, j_name, data in out_arcs:
+                j = self.all_nodes_indices[j_name]
+                for k in range(self.num_trucks):
+                    for d in range(self.num_drones):
+                        # cons 3
+                        rhs = ak_list[(s, k)] - M * (1 - z_list[(s, j, k, d)])
+                        self.constraints.append(model.addConstr(ad_list[(s, d)] >= rhs, f"sync3_{id}"))
+                        id += 1
+                        # cons 4
+                        travel_time = data["travel_time"]["truck"]
+                        rhs = ad_list[(s, d)] + travel_time - M * (1 - z_list[(s, j, k, d)])
+                        self.constraints.append(model.addConstr(ak_list[(j, k)] >= rhs, f"sync4_{id}"))
+                        id += 1
+
+        # test
+        # self.constraints.append(model.addConstr(z_list[(2, 3, 0, 0)] == 1))
+        # self.constraints.append(model.addConstr(y_list[(3, 5, 0)] == 1))
+        # self.constraints.append(model.addConstr(x_list[(3, 7, 0)] == 1))
+        # self.constraints.append(model.addConstr(z_list[(7, 8, 0, 0)] == 1))
+
+        # self.constraints.append(model.addConstr(y_list[(3, 1, 0)] == 1))
+        # self.constraints.append(model.addConstr(y_list[(1, 4, 0)] == 1))
+
         model.update()
+        model.write("model.lp")
         model.optimize()
         if model.status == GRB.OPTIMAL:
             print(f"Optimal solution found")
         elif model.status == GRB.INFEASIBLE:
-            print("No feasible solution found")
-            model.computeIIS()  # Find the irreducible infeasible set (IIS)
+            model.computeIIS()
             model.write("model.ilp")
+            print("No feasible solution found")
         elif model.status == GRB.UNBOUNDED:
             print("The model is unbounded")
 
         # Retrieve the objective value
         if model.status == GRB.OPTIMAL:
             print(f"Objective value: {model.objVal}")
-
-        # Extract and store the solution values for the decision variables
-        self.x_values = {(i, j, k): var.X for (i, j, k), var in x_list.items()}
-        self.y_values = {(i, j, d): var.X for (i, j, d), var in y_list.items()}
-        self.f_values = {(i, j, d): var.X for (i, j, d), var in f_list.items()}
-        self.z_values = {(i, j, k, d): var.X for (i, j, k, d), var in z_list.items()}
-        self.u_values = {s: var.X for s, var in u_list.items()}
-        self.t_values = {(n, d): var.X for (n, d), var in t_list.items()}
-        self.w_values = {(n, d): var.X for (n, d), var in w_list.items()}
-        self.v_values = {(n, d): var.X for (n, d), var in v_list.items()}
-        self.c_values = {(n, k): var.X for (n, k), var in c_list.items()}
+            # Extract and store the solution values for the decision variables
+            self.x_values = {(i, j, k): var.X for (i, j, k), var in x_list.items()}
+            self.y_values = {(i, j, d): var.X for (i, j, d), var in y_list.items()}
+            self.f_values = {(i, j, d): var.X for (i, j, d), var in f_list.items()}
+            self.z_values = {(i, j, k, d): var.X for (i, j, k, d), var in z_list.items()}
+            self.u_values = {s: var.X for s, var in u_list.items()}
+            self.t_values = {(n, d): var.X for (n, d), var in t_list.items()}
+            self.w_values = {(n, d): var.X for (n, d), var in w_list.items()}
+            self.v_values = {(n, d): var.X for (n, d), var in v_list.items()}
+            self.c_values = {(n, k): var.X for (n, k), var in c_list.items()}
+            self.ak_value = {(n, k): var.X for (n, k), var in ak_list.items()}
+            self.ad_value = {(n, d): var.X for (n, d), var in ad_list.items()}
+            sad = 0
 
     def visualize_routes(self):
         pos = nx.spring_layout(self.G, seed=self.seed)  # Use the same layout for consistency
